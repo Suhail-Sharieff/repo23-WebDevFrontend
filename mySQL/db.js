aa425d26e173d86aac36a5ab4ec9fd5776227caf
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-08-02 00:28:05 +0530

    zzzz

--- mySQL/db.js
+++ mySQL/db.js
@@ -7,7 +7,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Suhail@786',
+    password: '---',
     database: 'demoDB'
 });
 
