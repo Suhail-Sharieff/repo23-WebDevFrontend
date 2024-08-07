d2bd2c52c69e96ca81b15ab5232693e3cb7edb02
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-08-08 00:12:33 +0530

    improved LoopEx

--- learning vite+react/vite-project/src/components/LoopEx.jsx
+++ learning vite+react/vite-project/src/components/LoopEx.jsx
@@ -6,14 +6,17 @@ function LoopEx() {
         [
             { title: "Task1", desc: "This should be done first" },
             { title: "Task2", desc: "This should be done next" },
-            { title: "Task3", desc: "This should be done last" }
+            { title: "Task3", desc: "This should be done last" },
+            { title: "Task4", desc: "This should be done final" },
+            { title: "Task5", desc: "This should be done semifinal" },
+            { title: "Task6", desc: "This should be done superfinal" },
         ]
     );
 
     const TaskCard = ({prop}) => {
         return (
             <>
-                <div className="card">
+                <div className="card py-4 px-4">
 
                     <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
@@ -28,9 +31,9 @@ function LoopEx() {
 
 
     return (
-        <div className='flex py-4 justify-evenly'>
+        <div className='grid grid-cols-3 '>
             {/* loop */}
-            {task.map((taskItem) => (
+            {task.map((taskItem) => (//key needs to be given
                 <TaskCard key={taskItem.title} prop={taskItem} />
             ))}
         </div>
